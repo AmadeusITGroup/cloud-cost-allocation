12f6297d4478eee9bc443d8bf060935866a6d95f
Marc PERREAUT <marc.perreaut@amadeus.com>
2022-08-05 16:51:38 +0200

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme_file:
 # Setup
 setup(
     name='cloud-cost-allocation',
-    version='0.2.0',
+    version='0.2.1',
     description='Python library for shared, hierarchical cost allocation based on user-defined metrics.',
     long_description=readme,
     long_description_content_type='text/markdown',
